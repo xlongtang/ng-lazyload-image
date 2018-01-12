7de1e8e4169ffe74b930a70780a8c220255a8206
Xiaolong Tang <xxlongtang@gmail.com>
2018-01-12 17:11:31 -0500

    dist

---
+++ dist/src/lazyload-image.directive.d.ts
@@ -0,0 +1,26 @@
+import 'rxjs/add/operator/let';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/debounceTime';
+import { AfterContentInit, ElementRef, EventEmitter, NgZone, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+export interface IOnLoadedPayload {
+    success: boolean;
+    element: ElementRef;
+}
+export declare class LazyLoadImageDirective implements OnChanges, AfterContentInit, OnDestroy {
+    lazyImage: any;
+    defaultImage: string;
+    errorImage: string;
+    scrollTarget: Window;
+    scrollObservable: any;
+    offset: number;
+    onLoad: EventEmitter<IOnLoadedPayload>;
+    private propertyChanges$;
+    private elementRef;
+    private ngZone;
+    private platformId;
+    private scrollSubscription;
+    constructor(el: ElementRef, ngZone: NgZone);
+    ngOnChanges(changes?: SimpleChanges): void;
+    ngAfterContentInit(): any;
+    ngOnDestroy(): void;
+}
